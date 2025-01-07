86113bf26bd54955adf29a0982e73eee2b16cb37
BrutusGamesTeam <oficiallazarentos@gmail.com>
2025-01-07 14:52:49 -0300

    Create SimplexNoise.java

---
+++ Scripts/SimplexNoise.java
@@ -0,0 +1,16 @@
+package JAVARuntime;
+
+import JAVARuntime.Texture;
+import JAVARuntime.Vector2;
+
+// Classe Gerenciada no Lado do Servidor
+
+public class SimplexNoise
+{
+    public static Texture noise(int w, int h)
+    {
+        Texture t = new Texture().white();
+
+        return t;
+    }
+}
